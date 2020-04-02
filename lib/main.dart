19ea8b3dad0e3fa16e1d261622d89d5be78d0789
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-02 22:20:50 +0530

    Stateless and Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        "Hello, Damindu",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-
-        ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      backgroundColor: Colors.red[600],
-      child: Text("Click"),
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello, Damindu",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+
+          ),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.red[600],
+        child: Text("Click"),
+      ),
+    );
+  }
+}
