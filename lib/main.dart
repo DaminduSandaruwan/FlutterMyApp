6842996dada2d39cdf67f7b4048dab2ba5aa71a9
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-02 19:53:45 +0530

    Scaffold & AppBar Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hey, You!"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Hello Damindu"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("Click"),
+    ),
+  ),
 ));
