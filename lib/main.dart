61eab9444aa5d6a532aff0aba63d6bd5ad578314
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-02 21:45:38 +0530

    Colors & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,25 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('My First App'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text("Hello Damindu"),
+      child: Text(
+        "Hello, Damindu",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+
+        ),
+      ),
+
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      backgroundColor: Colors.red[600],
       child: Text("Click"),
     ),
   ),
