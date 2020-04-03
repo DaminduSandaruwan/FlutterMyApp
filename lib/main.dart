d84866e161df72f23487dfb380aa582dc083c287
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-03 23:03:51 +0530

    Columns - main axis and cross axis

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,30 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Row(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center ,
         children: <Widget>[
-          Text("Hello World"),
-          RaisedButton.icon(
-            onPressed: (){},
-            icon: Icon(
-                Icons.mail
-            ),
-            label: Text('Mail Me'),
-            color: Colors.amber,
+          Row(
+            children: <Widget>[
+              Text("Damindu"),
+              Text("Sandaruwan"),
+            ],
           ),
-
           Container(
+            padding: EdgeInsets.all(20.0),
             color: Colors.cyan,
+            child: Text("One"),
+          ),
+          Container(
             padding: EdgeInsets.all(30.0),
-            child: Text('Inside Container'),
+            color: Colors.amber,
+            child: Text("Two"),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.red,
+            child: Text("Three"),
           ),
         ],
       ),
