9f3b3e3214bdf23ba04a631ce3f5e7c49167b334
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-03 21:00:23 +0530

    Buttons and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,44 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/img2.jpg'),
+        child: IconButton(
+          onPressed: (){
+            print('You Clicked Me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
+
+//        child: RaisedButton.icon(
+//          onPressed: (){},
+//          icon: Icon(
+//            Icons.mail
+//          ),
+//          label: Text('Mail Me'),
+//          color: Colors.amber,
+//        )
+
+//        child: FlatButton(
+//          onPressed: (){
+//            print('You Clicked Me');
+//          },
+//          child: Text('Click Me'),
+//          color: Colors.lightBlue,
+//        ),
+
+//        child: RaisedButton(
+//          onPressed: (){},
+//          child: Text('Click Me'),
+//          color: Colors.lightBlue,
+//        ),
+
+//        child: Icon(
+//          Icons.airport_shuttle,
+//          color: Colors.lightBlue,
+//          size: 50.0,
+//        ),
+
+//        child: Image.asset('assets/img2.jpg'),
 //        child: Image(
 //          image: AssetImage('assets/img3.jpg'),
 //          image: NetworkImage('https://images.unsplash.com/photo-1506598417715-e3c191368ac0?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
