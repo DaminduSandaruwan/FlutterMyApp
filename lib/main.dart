794c99f6113d04f13639d7e70326f347a014ba57
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-03 22:12:52 +0530

    Container and Padding

--- lib/main.dart
+++ lib/main.dart
@@ -14,61 +14,21 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: (){
-            print('You Clicked Me');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
-
-//        child: RaisedButton.icon(
-//          onPressed: (){},
-//          icon: Icon(
-//            Icons.mail
-//          ),
-//          label: Text('Mail Me'),
-//          color: Colors.amber,
-//        )
-
-//        child: FlatButton(
-//          onPressed: (){
-//            print('You Clicked Me');
-//          },
-//          child: Text('Click Me'),
-//          color: Colors.lightBlue,
-//        ),
-
-//        child: RaisedButton(
-//          onPressed: (){},
-//          child: Text('Click Me'),
-//          color: Colors.lightBlue,
-//        ),
-
-//        child: Icon(
-//          Icons.airport_shuttle,
-//          color: Colors.lightBlue,
-//          size: 50.0,
-//        ),
-
-//        child: Image.asset('assets/img2.jpg'),
-//        child: Image(
-//          image: AssetImage('assets/img3.jpg'),
-//          image: NetworkImage('https://images.unsplash.com/photo-1506598417715-e3c191368ac0?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
-//        ),
-
-//        child: Text(
-//          "Hello, Damindu",
-//          style: TextStyle(
-//            fontSize: 20.0,
-//            fontWeight: FontWeight.bold,
-//            letterSpacing: 2.0,
-//            color: Colors.grey[600],
-//            fontFamily: 'IndieFlower',//
-//          ),
-//        ),
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child:  Text('Hello'),
       ),
+//      body: Container(
+//        padding: EdgeInsets.fromLTRB(10, 20, 20, 10),
+//        margin: EdgeInsets.all(30.0),
+//
+//
+//   //     padding: EdgeInsets.all(20.0),
+//   //     padding : EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+//        color: Colors.grey[400],
+//        child: Text('Hello'),
+//      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         backgroundColor: Colors.red[600],
