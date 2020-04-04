6413c9c644d68666e5d7abc0d858e2b40574c1bc
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-04 20:12:07 +0530

    Expanded Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,34 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center ,
+      body: Row(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text("Damindu"),
-              Text("Sandaruwan"),
-            ],
+          Expanded(
+            flex: 3,
+              child: Image.asset('assets/img1.jpg')),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text("1"),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.cyan,
-            child: Text("One"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text("2"),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.amber,
-            child: Text("Two"),
-          ),
-          Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.red,
-            child: Text("Three"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.red,
+              child: Text("3"),
+            ),
           ),
         ],
       ),
